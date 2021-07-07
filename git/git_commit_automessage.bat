fe239436e81193e52adc8806ef9e9ab89a004f24
lostjarred <lostotaku@outlook.com>
2021-07-07 16:54:58 +1000

    add git commit auto message script

---
+++ git/git_commit_automessage.bat
@@ -0,0 +1,12 @@
+@ECHO off
+SET "$datetime=%time% %date%"
+ECHO the time and date is
+ECHO %$datetime%
+SET "$message=REPLACE ME YOU DOLT"
+ECHO The message is
+ECHO %$message%
+SET "$autocommitmsg=%$message% %$datetime%"
+ECHO The auto commit mesage is
+ECHO %$autocommitmsg%
+git commit --message=%$autocommitmsg%
+
